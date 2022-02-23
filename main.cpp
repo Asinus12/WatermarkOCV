72fd94063e5c3622f5720984705e8d6b5d701399
Asinus <b.bogataj23@gmail.com>
2022-02-23 12:11:39 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+     Mat main_image,watermark, watermarked_image;
+  string file;
+  double alpha,beta;
+  /* Enter path of the file if file is not in the directory of the project */
+  cout << "\nEnter name of a main image file with extension: ";
+  /* cin function cannot read space. Hence we have used getline function here. */
+  getline(cin, file);
+  /* imread() is used to read an image */
+  main_image = imread(file);
+  /* Checking if main_image object contains an image */
+  if (main_image.data==0)
+  {
+    cout << "\n\nError..!!\n";
+    cout << "\nCheck=>";
+    cout << "\n1. Desired image is in the folder of the project";
+    cout << "\n2. You have mentioned extension of an image file\n";
+    exit(0);
+  }
+
+
+//   cout << "\nEnter name of a watermark file with extension: ";
+//   getline(cin, file);
+  watermark = imread("w.png");
+  /* Checking if watermark object contains an image */
+//   if (watermark.data == 0)
+//   {
+//     cout << "\n\nError..!!\n";
+//     cout << "\nCheck=>";
+//     cout << "\n1. Desired image is in the folder of the project";
+//     cout << "\n2. You have mentioned extension of an image file\n";
+//     exit(0);
+//   }
+  /* Checking if both files have equal resolution or not */
+  if (main_image.rows != watermark.rows || main_image.cols != watermark.cols)
+  {
+    cout << "\n\nError..!!\n";
+    cout << "\nResolution of both images is not same\n";
+    exit(0);
+  }
+  /* alpha denotes the opacity of the watermark and beta denotes the opacity of the main_image. You can choose alpha and beta values as per your choice but make sure that the sum of alpha and beta is 1. Here we have used alpha as 0.1 and beta as 0.9 */
+  alpha = 0.1;
+  beta = 1 - alpha;
+  /* Here we are merging 'main_image' and 'watermark' and output will be stored in 'watermarked_image' */
+  addWeighted(watermark, alpha, main_image, beta, 0.0, watermarked_image);
+  /* namedWindow() is used to create a window with the mentioned name. In our case, window name is 'Watermarked image' */
+  namedWindow("Watermarked image", WINDOW_AUTOSIZE);
+  /* imshow() is used to display an image */
+  imshow("Watermarked image", watermarked_image);
+  /* Press esc to exit */
+  if (waitKey(0) == 27)
+  {
+    exit(0);
+  }
+  /* when everything done, close all windows */
+  destroyAllWindows();
+}
